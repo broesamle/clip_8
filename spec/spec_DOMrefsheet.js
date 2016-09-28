cb30871f82dd88613c1972bc61b448fda64bc46d
Martin Brösamle <m@m-broe.de>
2016-09-28 10:49:39 +0200

    Add custom matchers; catch missing DOM nodes.

--- spec/spec_DOMrefsheet.js
+++ spec/spec_DOMrefsheet.js
@@ -1,19 +1,42 @@
 
+var customMatchers = {
+toBeElement:
+    function (util, customEqualityTesters) {
+        return {
+            compare: function(actual, expected) {
+                var result = {};
+                result.pass = (actual instanceof SVGElement) || (actual instanceof HTMLElement);
+                result.message = "Expected " + actual + " to be instance of HTMLElement or SVGElement.";
+                return result;
+            }
+        };
+    },
+toMatchReference:
+    function (util, customEqualityTesters) {
+        return {
+            compare: function(actual, expected) {
+                var result = {};
+                result.pass = actual.isEqualNode(expected);
+                result.message = "Expected " + actual + " to equal " + expected + ".";
+                return result;
+            }
+        };
+    }
+};
+
 var test_specids = [];
 var test_domids = [];
-
 var visualReporter = {
 specDone: function(result) {
         var domid = test_domids.shift();
         var specid = test_specids.shift();
-        console.log("REPORT["+result.id+"]", "fetched ids:", test_specids.shift(), domid,' was ' + result.status);
         var reportnode = document.createElement("span");
-        if (result.status == "passed") { 
-            reportnode.appendChild(document.createTextNode("•")); 
+        if (result.status == "passed") {
+            reportnode.appendChild(document.createTextNode("•"));
             reportnode.setAttribute("class", "DOMreftest-passed");
         }
-        else if (result.status == "failed") { 
-            reportnode.appendChild(document.createTextNode("×")); 
+        else if (result.status == "failed") {
+            reportnode.appendChild(document.createTextNode("×"));
             reportnode.setAttribute("class", "DOMreftest-failed");
         }
         else { reportnode.appendChild(document.createTextNode("INTERNAL ERROR!")); }
@@ -30,15 +53,30 @@ function getTestDOM(reftestElement) { return reftestElement.firstElementChild.ne
 
 function addTest_invokeOperation(reftestElement) {
     var spec;
+
+    spec = it("["+reftestElement.id+"] PRE, POST, TEST should contain HTML or SVG elements.", function(done) {
+        var pre = getPrecondition(reftestElement);
+        var post = getPostcondition(reftestElement);
+        var proc = getTestDOM(reftestElement);
+        expect(pre.firstElementChild).toBeElement();
+        expect(post.firstElementChild).toBeElement();
+        expect(p.firstElementChild).toBeElement();
+        done();
+    });
+
+    console.log("addTest_invokeOperation: sepc:", spec.id, reftestElement.id);
+    test_specids.push(spec.id);
+    test_domids.push(reftestElement.id);
+
     spec = it("["+reftestElement.id+"] PRE and TEST should be equal", function(done) {
         var pre = getPrecondition(reftestElement);
         var proc = getTestDOM(reftestElement);
-
         expect(pre.classList).toContain("pre-reference");
         expect(proc.classList).toContain("testDOM");
-        expect(pre.firstElementChild.isEqualNode(proc.firstElementChild)).toBeTruthy();
+        expect(proc.firstElementChild).toMatchReference(pre.firstElementChild);
         done();
     });
+
     console.log("addTest_invokeOperation: sepc:", spec.id, reftestElement.id);
     test_specids.push(spec.id);
     test_domids.push(reftestElement.id);
@@ -48,7 +86,7 @@ function addTest_invokeOperation(reftestElement) {
         expect(proc.classList).toContain("testDOM");
         var svgroot = proc.firstElementChild;
         var oldId = svgroot.id;
-        svgroot.id = "clip8svgroot";
+        svgroot.setAttributeNS(null,"id", "clip8svgroot");
         expect(svgroot.id).toBe("clip8svgroot");
         clip8envokeOperation();
         svgroot.removeAttribute("id", reftestElement.id);
@@ -64,7 +102,7 @@ function addTest_invokeOperation(reftestElement) {
 
         expect(proc.classList).toContain("testDOM");
         expect(post.classList).toContain("post-reference");
-        expect(proc.firstElementChild.isEqualNode(post.firstElementChild)).toBeTruthy();
+        expect(proc.firstElementChild).toMatchReference(post.firstElementChild);
         done();
     });
     console.log("addTest_invokeOperation: sepc:", spec.id, reftestElement.id);
@@ -73,6 +111,10 @@ function addTest_invokeOperation(reftestElement) {
 }
 
 describe("Reference Sheet Tester", function(){
+    beforeEach(function() {
+        jasmine.addMatchers(customMatchers);
+    });
+
     var  tests = document.getElementsByClassName("DOMreftest");
     for (var i = 0; i < tests.length; i++) {
         addTest_invokeOperation(tests[i]);
