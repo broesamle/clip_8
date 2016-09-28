4b9faf44f233bc4cc714615c401f6e4323dda833
Martin Brösamle <m@m-broe.de>
2016-09-28 14:59:18 +0200

    Make envokeOperation throw if non-svg root.

--- js/selftest_pseudoclip8-rmOP.js
+++ js/selftest_pseudoclip8-rmOP.js
@@ -1,4 +1,5 @@
 function clip8envokeOperation() {
     var svgroot = document.getElementById("clip8svgroot");
     console.log("clip8envokeOperation:", svgroot);
+    if (! (svgroot instanceof SVGElement)) { throw "[clip8] no SVG root."; }
 }
