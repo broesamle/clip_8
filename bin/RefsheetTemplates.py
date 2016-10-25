7d51d3608b0b93d72e8b921b4b9c7c31fdb81076
Martin Brösamle <m@m-broe.de>
2016-10-25 11:55:50 +0200

    First draft for reference sheet templates.

---
+++ bin/RefsheetTemplates.py
@@ -0,0 +1,65 @@
+import PyBroeModules.MultiTemplateA as MT
+from string import Template
+
+Document = Template("""
+<!DOCTYPE html>
+<html>
+$HEADER
+$BODY
+</html>
+""")
+
+Header = Template("""
+<head>
+<meta charset="utf-8">
+<title>$refsheettitle</title>
+
+<link rel="shortcut icon" type="image/png" href="../lib/jasmine-2.5/jasmine_favicon.png">
+<link rel="stylesheet" href="../lib/jasmine-2.5/jasmine.css">
+<link rel="stylesheet" href="../css/refsheet.css">
+<link rel="stylesheet" href="../css/clip8.css">
+
+<script src="../lib/jasmine-2.5/jasmine.js"></script>
+<script src="../lib/jasmine-2.5/jasmine-html.js"></script>
+<script src="../lib/jasmine-2.5/boot.js"></script>
+<script src="../js/SVGPaper-align.js"></script>
+<script src="../js/clip8decode.js"></script>
+<script src="../js/clip8.js"></script>
+</head>
+""")
+
+Body = Template("""
+<body>
+<h1>clip_8: SVG Paper Operations, Reference Sheet A</h1>
+$TESTSECTIONS
+<script src="../spec/spec_DOMrefsheet.js"></script>
+</body>
+""")
+
+Testsection = Template("""
+<h3>$testsectiontitle</h3>
+$TESTS
+""")
+
+SingleReferenceTest = Template("""
+<div>$testdescription</div>
+<p class="DOMreftest envokeOperation" id="$testid">
+<span class="pre-reference">
+<svg viewbox="-2 -2 68 68" width="68" height="68">
+$pre-reference
+</svg>
+</span>
+&nbsp;==&gt;&nbsp;
+<span class="post-reference">
+<svg viewbox="-2 -2 68 68" width="68" height="68">
+$post-reference
+</svg>
+</span>
+&nbsp;:&nbsp;&nbsp;&nbsp;
+<span class="testDOM">
+<svg viewbox="-2 -2 68 68" width="68" height="68">
+$testDOM
+</svg>
+</span>
+</p>
+""")
