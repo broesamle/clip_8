aaf4c9d8de1b3b20121cdfe19a67303d13edd697
Martin Brösamle <m@m-broe.de>
2016-10-27 21:49:02 +0200

    Transplants SVG layers into HTML ref sheet for testing.

--- bin/svg2refsheet.py
+++ bin/svg2refsheet.py
@@ -5,6 +5,9 @@ from PyBroeModules.StripNamespace import stripNamespace
 
 import RefsheetTemplates as TEM
 
+def allChildrenToSVG(el):
+    return u"".join ( [ stripNamespace(ET.tostring(child, encoding='utf-8').decode('utf8')) for child in el ] )
+
 class XMLNodesCollection(ItemsCollection):
     """ Read a number of nodes from an XML file"""
     def __init__(self,filename,elementXpath,namespaces={},reverse=False,**kwargs):
@@ -18,7 +21,6 @@ class XMLNodesCollection(ItemsCollection):
         """ PLEASE OVERLOAD! The default implementation derives the key from the node id and adds the xml in the field `XML_CONTENT`."""
         raise NotImplementedError()
 
-
 class SVGGroupCollection(XMLNodesCollection):
     def __init__(self, filename, idprefix, *args, **kwargs):
         self.prefix = idprefix
@@ -28,19 +30,34 @@ class SVGGroupCollection(XMLNodesCollection):
         """ Check whether the id is valid, and then generate a key for the item from it.
             The default case checks the presence of the prefix and removes it. """
         if id.startswith(self.prefix):
-            return id[len(self.prefix):]
+            result = id[len(self.prefix):]
         else:
             raise ValueError("Invalid ID: "+id+" pre:"+self.prefix)
+        return result
 
     def processElement(self, el):
         try:
             id = self.keyFromId(el.get('id',""))
             newitem = {}
-            newitem['pre'] = stripNamespace(ET.tostring(el,encoding='utf8',method='xml'))
+            for child in el:
+                if child.get('id',"").startswith("t0"):
+                    newitem['pre'] = allChildrenToSVG(child)
+                    newitem['testDOM'] = allChildrenToSVG(child)
+                elif child.get('id',"").startswith("t1"):
+                    newitem['post'] = allChildrenToSVG(child)
+                elif child.get('id',"").startswith("I"):
+                    newitem['pre'] += allChildrenToSVG(child)
+                    newitem['post'] += allChildrenToSVG(child)
+                    newitem['testDOM'] += allChildrenToSVG(child)
+                else:
+                    print("WARNING: Encountered invalid sublayer %s in test %s." % (child.get('id',"--unknown--"),id))
+            newitem['testid'] = id
             ## TODO: extract correct bits from SVG and define `pre`, `post` and `testDOM`.
             self.addItem(id, newitem)
         except ValueError:
             pass
+
+
 
 tests = SVGGroupCollection(
     os.path.join("..","svg","TEST-prototypeAI1.svg"),
