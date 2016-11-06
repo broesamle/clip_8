1829de0fc112510d77617add9035820d8975ffde
Martin Brösamle <m@m-broe.de>
2016-11-07 00:10:11 +0100

    Identifies initial node, init 1st instruction OK.

--- js/clip8.js
+++ js/clip8.js
@@ -1,41 +1,50 @@
 "use strict";
 
-function clip8getClassListSVG(el) {
-    return el.getAttributeNS(null, "class").split(' ');
+function clip8initControlFlow(svgroot) {
+    var debug = false;
+    var epsilon = 1;
+    var circles = svgroot.getElementsByTagName("circle");
+    var centres = svgdom_addGroup(svgroot);
+    var initialflow = null;
+    if (debug) var debugvis = svgdom_addGroup(svgroot);
+
+    for ( var i = 0; i < circles.length; i++ ) {
+        var r = svgdom_CentreArea(circles[i], epsilon);
+        r.setAttribute("fill", "#ffff33");
+        centres.appendChild(r);
+    }
+    for ( var i = 0; i < centres.childNodes.length; i++ ) {
+        var sel = svgretrieve_selectorFromRect(centres.childNodes[i], svgroot);
+        if (debug) {
+            console.log("clip8initControlFlow: selector", sel);
+            var debugrect = svgdom_addRect(debugvis, sel.x, sel.y, sel.width, sel.height);
+            debugrect.setAttribute("stroke", "#000000");
+            debugrect.setAttribute("stroke-width", "0.5");
+            debugrect.setAttribute("fill", "none");
+            debugrect.setAttribute("pointer-events", "none");
+        }
+        var hitlist = svgroot.getIntersectionList(sel, centres);
+        if (debug)  console.log("clip8initControlFlow: hitlist", hitlist);
+        if (hitlist.length == 1) {
+            var initarea = svgretrieve_selectorFromRect(hitlist[0], svgroot);
+            svgroot.removeChild(centres);
+            hitlist = svgroot.getIntersectionList(initarea, svgroot);
+            if (debug)  console.log("clip8initControlFlow: initiallocation", hitlist);
+            break;
+        }
+    }
+    if (debug) console.log("clip8initControlFlow: els at initial location.", hitlist);
+    for ( var i = 0; i < hitlist.length; i++ )
+        if (hitlist[i].tagName == "path") return hitlist[i];
+    throw "failed to idendify point of entry."
 }
 
 function clip8envokeOperation() {
     var svgroot = document.getElementById("clip8svgroot");
     console.log("clip8envokeOperation:", svgroot);
     if (!(svgroot instanceof SVGElement)) { throw "[clip8] no SVG root."; }
-    var oper = [];
-    for ( var i = 0; i < svgroot.childNodes.length; i++ ) {
-        if ( svgroot.childNodes[i] instanceof Element && svgroot.childNodes[i].classList.contains("clip8OP") ) {
-            oper.push(svgroot.childNodes[i]);
-        }
-    }
-    console.log("OPS:", oper, oper.length);
-    if (oper.length == 0) { throw "clip8: no operation found."; }
-    for ( var i = 0; i < oper.length; i++ ) {
-        svgroot.removeChild(oper[i]);
-    }
 
-    var els = [];
-    for ( var i = 0; i < svgroot.childNodes.length; i++ )
-        if ( svgroot.childNodes[i] instanceof SVGElement )
-            els.push(svgroot.childNodes[i]);
-    console.log("ELS:", els, els.length);
-
-    if ( oper[0].classList.contains("clip8alignrel") ) {
-        if (!(oper[0] instanceof SVGLineElement)) { throw "[clip8] OP alignrel must be SVGLineElement."; }
-        switch (clip8directionOfSVGLine(oper[0])) {
-            case "top":     paperclip_alignrelTop(els); break;
-            case "bottom":  paperclip_alignrelBottom(els); break;
-            case "left":    paperclip_alignrelLeft(els); break;
-            case "right":   paperclip_alignrelRight(els); break;
-        }
-    }
-    else if ( oper[0].classList.contains("clip8cut") ) {
-        throw "clip8cut not implemented.";
-    }
+    svgdom_setSVGNS(svgroot.namespaceURI);
+    var ip = clip8initControlFlow(svgroot);
+    console.log("clip8envokeOperation: IP", ip);
 }
