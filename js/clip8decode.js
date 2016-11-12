15fefd13aa6b99dd428d1b9c4bde0e11ccfaf5af
Martin Brösamle <m@m-broe.de>
2016-11-12 16:55:29 +0100

    Make clip8directionOfSVGLine consider epsilon and minlen.

--- js/clip8decode.js
+++ js/clip8decode.js
@@ -1,15 +1,17 @@
+function clip8directionOfSVGLine(line, epsilon, minlen) {
+    if (!(line instanceof SVGLineElement)) { throw "[clip8directionOfSVGLine] expected line element."; }
+    if (!epsilon) { throw "[clip8directionOfSVGLine] expected epsilon to be a number > 0"; }
+    if (!minlen) { throw "[clip8directionOfSVGLine] expected minlen to be a number > 0"; }
 
-function clip8directionOfSVGLine(line) {
-    if          (line.x1.baseVal.value == line.x2.baseVal.value) {
-        // exactly vertical
-        if      (line.y1.baseVal.value < line.y2.baseVal.value) return "bottom";
-        else if (line.y1.baseVal.value > line.y2.baseVal.value) return "top";
-        else throw "[clip8] invalid direction of line (a).";
-    } else if   (line.y1.baseVal.value == line.y2.baseVal.value) {
-        // exactly horizontal
-        if      (line.x1.baseVal.value < line.x2.baseVal.value) return "right";
-        else if (line.x1.baseVal.value > line.x2.baseVal.value) return "left";
-        else throw "[clip8] invalid direction of line (b).";
-    } else throw "[clip8] Direction of non-vertical or non-horizontal lines not implemented.";
-    return "vertical"
+    var deltax = line.x2.baseVal.value - line.x1.baseVal.value;
+    var deltay = line.y2.baseVal.value - line.y1.baseVal.value;
+    if ( Math.abs(deltax) < epsilon && Math.abs(deltay) > minlen )
+        // vertical
+        if (deltay > 0)     return 'DOWN';
+        else                return 'UP';
+    else if ( Math.abs(deltay) < epsilon && Math.abs(deltax) > minlen )
+        // horizontal
+        if (deltax > 0)     return 'RIGHT';
+        else                return 'LEFT';
+    else throw "[clip8] Direction of non-vertical or non-horizontal lines not implemented.";
 }
