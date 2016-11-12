e9c5ba9b1416a4a1cc7ee28054a7afd7a6815488
Martin Brösamle <m@m-broe.de>
2016-11-12 18:17:33 +0100

    ATagname counting for a more generic instruction decoder.

--- js/clip8decode.js
+++ js/clip8decode.js
@@ -15,3 +15,24 @@ function clip8directionOfSVGLine(line, epsilon, minlen) {
         else                return 'LEFT';
     else throw "[clip8] Direction of non-vertical or non-horizontal lines not implemented.";
 }
+
+function clip8countTags(parentelement, tagnames) {
+    /** How many child elements are present for a list of tagnames.
+    *   `tagnames` is a list of strings, each element representing a tagname to count.
+    *   Returns a list of integers, each element representing the count of the corresponding tagname in `tagnames`.
+    */
+    var debug = false;
+    var result = [];
+    var currenttagname, currentcount;
+    for ( var j = 0; j < tagnames.length; j++ ) {
+        currenttagname = tagnames[j];
+        currentcount = 0;
+        for ( var i = 0; i < parentelement.childNodes.length; i++ ) {
+            if (debug) console.log("clip8countTags: search for", currenttagname, "==",parentelement.childNodes[i].tagName);
+            if (parentelement.childNodes[i].tagName == currenttagname) currentcount++;
+        }
+        if (debug) console.log("clip8countTags: count", currentcount);
+        result.push(currentcount);
+    }
+    return result;
+}
