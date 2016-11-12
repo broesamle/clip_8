a21efad79fb53af745192f6e3b0d42d3d8965826
Martin Brösamle <m@m-broe.de>
2016-11-12 12:55:09 +0100

    Highlight execution trace in a separate SVG element. clone  into  and add visualisation elements there. a timer fades out the trace step by step, and finally removes them.

--- js/clip8.js
+++ js/clip8.js
@@ -1,12 +1,11 @@
 "use strict";
 
-function clip8initControlFlow(svgroot) {
+function clip8initControlFlow(svgroot, tracesvgroot) {
     var debug = false;
     var epsilon = 1;
     var circles = svgroot.getElementsByTagName("circle");
     var centres = svgdom_addGroup(svgroot);
     var initialflow = null;
-    if (debug) var debugvis = svgdom_addGroup(svgroot);
 
     for ( var i = 0; i < circles.length; i++ ) {
         var r = svgdom_CentreArea(circles[i], epsilon);
@@ -15,18 +14,13 @@ function clip8initControlFlow(svgroot) {
     }
     for ( var i = 0; i < centres.childNodes.length; i++ ) {
         var sel = svgretrieve_selectorFromRect(centres.childNodes[i], svgroot);
-        if (debug) {
-            console.log("clip8initControlFlow: selector", sel);
-            var debugrect = svgdom_addRect(debugvis, sel.x, sel.y, sel.width, sel.height);
-            debugrect.setAttribute("stroke", "#000000");
-            debugrect.setAttribute("stroke-width", "0.5");
-            debugrect.setAttribute("fill", "none");
-            debugrect.setAttribute("pointer-events", "none");
-        }
         var hitlist = svgroot.getIntersectionList(sel, centres);
         if (debug)  console.log("clip8initControlFlow: hitlist", hitlist);
         if (hitlist.length == 1) {
             var initarea = svgretrieve_selectorFromRect(hitlist[0], svgroot);
+            // visualise initial control flow node
+            var tracerect = svgdom_addRect(tracesvgroot, initarea.x-3,initarea.y-3, initarea.width+6, initarea.height+6);
+            clip8setTraceAttribs(tracerect);
             svgroot.removeChild(centres);
             hitlist = svgroot.getIntersectionList(initarea, svgroot);
             if (debug)  console.log("clip8initControlFlow: initiallocation", hitlist);
@@ -60,12 +54,12 @@ function clip8getPrimInstruction (ip, svgroot) {
         if (hitlist[i].getAttribute("stroke-linecap") == "round" ||
             hitlist[i].tagName == "circle") {
             instr1.appendChild(hitlist[i].cloneNode(false));
-            instr1.lastElementChild.setAttribute("stroke", "#FFEE22");
+            instr1.lastElementChild.setAttribute("stroke", "#fff");
         }
         else if (hitlist[i].getAttribute("stroke-dasharray") &&
             hitlist[i].tagName == "rect") {
             sel.appendChild(hitlist[i].cloneNode(false));
-            sel.lastElementChild.setAttribute("stroke", "#FFEE22");
+            sel.lastElementChild.setAttribute("stroke", "#fff");
         }
     return [instr1,sel];
 }
@@ -77,7 +71,11 @@ function clip8envokeOperation() {
     if (!(svgroot instanceof SVGElement)) { throw "[clip8] no SVG root."; }
 
     svgdom_setSVGNS(svgroot.namespaceURI);
-    var ip = clip8initControlFlow(svgroot);     // instruction pointer: the active control flow path
+
+    var tracesvgroot = svgroot.cloneNode(false);
+    svgroot.parentNode.appendChild(tracesvgroot);
+    tracesvgroot.setAttribute("style", "margin-left:-64; background:none;");
+    var ip = clip8initControlFlow(svgroot, tracesvgroot);     // instruction pointer: the active control flow path
     var running = true;
     while (running) {
         if (debug) console.log("clip8envokeOperation: IP", ip);
@@ -109,6 +107,29 @@ function clip8envokeOperation() {
         }
         svgroot.removeChild(instr1);
         svgroot.removeChild(sel1);
+        tracesvgroot.appendChild(instr1);
+        tracesvgroot.appendChild(sel1);
         if (debug) console.log("clip8envokeOperation: removed instr1, sel1", instr1, sel1);
     }
+    var erasetracetimer = setInterval( function() { eraseTrace(tracesvgroot) }, 60 );
+    setTimeout ( function () { clearInterval(erasetracetimer) }, 10000 );   // stop erasor after some time
+}
+
+function eraseTrace (svgroot) {
+    var itemopacity;
+    for ( var i = 0; i < svgroot.childNodes.length; i++ ) {
+        itemopacity = svgroot.childNodes[i].getAttribute("opacity");
+        if (itemopacity == null)
+            svgroot.childNodes[i].setAttribute("opacity", "1.0");
+        else
+            if (itemopacity < 0.4) svgroot.removeChild(svgroot.childNodes[i]);
+            else svgroot.childNodes[i].setAttribute("opacity", itemopacity-0.04);
+    }
+}
+
+function clip8setTraceAttribs(el) {
+    el.setAttribute("stroke", "#88aaff");
+    el.setAttribute("stroke-width", "1");
+    el.setAttribute("fill", "none");
+    el.setAttribute("pointer-events", "none");
 }
