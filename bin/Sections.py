04d3703c06d624f74fb0eb277d7123481757b7fe
Martin Brösamle <m@m-broe.de>
2016-12-01 13:32:31 +0100

    New test section: move to; adapt titles.

--- bin/Sections.py
+++ bin/Sections.py
@@ -5,6 +5,7 @@ sections = [
     ("Iconic Instruction Language", "Selectors", 'test_selectors.svg'),
     ("Paper inspired Operations", "Align relative", 'test_alignrel.svg'),
     ("Paper inspired Operations", "Align absolute", 'test_alignabs.svg'),
-    ("Paper inspired Operations", "Move relative", 'test_moverel.svg'),
+    ("Paper inspired Operations", "Move by vector (relative)", 'test_moverel.svg'),
+    ("Paper inspired Operations", "Move to location (absolute)", 'test_moveabs.svg'),
     ("Paper inspired Operations", "Create and destroy", 'test_create+destroy.svg'),
     ("Changing dimensions", "Scale and resize", 'test_scale+resize.svg') ]
