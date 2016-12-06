c0efc361262f2ebb441ab0b2c259fe1749609033
Martin Brösamle <m@m-broe.de>
2016-12-06 21:15:18 +0100

    Fix errors in tests: exact positions order; two more pass.

--- bin/Sections.py
+++ bin/Sections.py
@@ -1,6 +1,6 @@
 from collections import OrderedDict
 
-refsheet_version = "0.0.2"
+refsheet_version = "0.0.3"
 refsheet_description = "encompasses a usable (incomplete) language repertoire."
 
 sections = [
@@ -16,7 +16,7 @@ sections = [
 expected_to_fail = [
     "alternative-join-combo", "alternative-join-combo2", "alternative-join-combo3", "alternative-join-combo4", "alternative-join-combo5",
     "enclose2", "intersect1", "intersect2", "indirect1", "indirect2", "indirect3",
-    "move-rel-v-down", "move-rel-dl", "move-bysize-down", "move-bysize-down2",
+    "move-bysize-down", "move-bysize-down2",
     "moveto-lowerleft", "moveto-lowerleft2", "moveto-upperleft",
     "clone1", "cut-vertical", "delete1", "delete2",
     "scale1", "shrink-upper", "grow-upper", "grow-right", "setheight", "setheight-indir"
