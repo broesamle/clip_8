e0d461852f64a1310021e749efb26dbc7a8b1c75
Martin Brösamle <m@m-broe.de>
2017-03-02 17:05:59 +0100

    Tests for different size/viewBox combinations.

--- spec/svgretrieve-spec.js
+++ spec/svgretrieve-spec.js
@@ -1,6 +1,32 @@
 describe("getEnclosedElements", function() {
     var svgroot;
 
+    var putandretrieve_circles = function () {
+        var cx, cy, circ, arearect, hitlist;
+
+        for (var i = 0; i<200; i++) {
+            expect(svgroot.firstChild).toBe(null);
+            cx = Math.random()*100.0;
+            cy = Math.random()*100.0;
+            circ = document.createElementNS(svgroot.namespaceURI,"circle");
+            circ.setAttribute("cx", cx);
+            circ.setAttribute("cy", cy);
+            circ.setAttribute("fill", "#000000;");
+            circ.setAttribute("r", 0.999);
+            svgroot.appendChild(circ);
+
+            arearect = svgroot.createSVGRect();
+            arearect.x = cx-1.0;
+            arearect.y = cy-1.0;
+            arearect.width = 2.0;
+            arearect.height = 2.0;
+            hitlist = Svgretrieve.getEnclosedElements(arearect, svgroot);
+            expect(hitlist.length).toBe(1, "to find exactly one circle in the hitlist");
+            expect(hitlist[0] instanceof SVGCircleElement).toBe(true, "retrieved element to be instance of SVGCircleElement");
+            svgroot.removeChild(svgroot.firstChild);
+        }
+    };
+
     beforeEach(function() {
         svgroot = document.getElementById("svgroot1");
         while (svgroot.firstChild) {
@@ -11,58 +37,29 @@ describe("getEnclosedElements", function() {
         svgroot.setAttribute("viewBox", "0 0 100 100");
     });
 
-    it("retreives elements from a SVG container", function() {
+    it("retreives elements from an SVG container", function() {
         expect(svgroot instanceof SVGElement).toBe(true);
-        var cx, cy, circ, arearect, hitlist;
-
-        for (var i = 0; i<200; i++) {
-            expect(svgroot.firstChild).toBe(null);
-            cx = Math.random()*100.0;
-            cy = Math.random()*100.0;
-            circ = document.createElementNS(svgroot.namespaceURI,"circle");
-            circ.setAttribute("cx", cx);
-            circ.setAttribute("cy", cy);
-            circ.setAttribute("fill", "#000000;");
-            circ.setAttribute("r", 0.999);
-            svgroot.appendChild(circ);
-
-            arearect = svgroot.createSVGRect();
-            arearect.x = cx-1.0
-            arearect.y = cy-1.0;
-            arearect.width = 2.0;
-            arearect.height = 2.0;
-            hitlist = Svgretrieve.getEnclosedElements(arearect, svgroot);
-            expect(hitlist.length).toBe(1);
-            expect(hitlist[0] instanceof SVGCircleElement).toBe(true);
-            svgroot.removeChild(svgroot.firstChild);
-        }
+        putandretrieve_circles();
     });
     it("retreives elements from a resized SVG container", function() {
         expect(svgroot instanceof SVGElement).toBe(true);
         svgroot.setAttribute("width", 150);
         svgroot.setAttribute("height", 150);
-        var cx, cy, circ, arearect, hitlist;
-
-        for (var i = 0; i<200; i++) {
-            expect(svgroot.firstChild).toBe(null);
-            cx = Math.random()*100.0;
-            cy = Math.random()*100.0;
-            circ = document.createElementNS(svgroot.namespaceURI,"circle");
-            circ.setAttribute("cx", cx);
-            circ.setAttribute("cy", cy);
-            circ.setAttribute("fill", "#000000;");
-            circ.setAttribute("r", 0.999);
-            svgroot.appendChild(circ);
-
-            arearect = svgroot.createSVGRect();
-            arearect.x = cx-1.0
-            arearect.y = cy-1.0;
-            arearect.width = 2.0;
-            arearect.height = 2.0;
-            hitlist = Svgretrieve.getEnclosedElements(arearect, svgroot);
-            expect(hitlist.length).toBe(1);
-            expect(hitlist[0] instanceof SVGCircleElement).toBe(true);
-            svgroot.removeChild(svgroot.firstChild);
-        }
+        putandretrieve_circles();
+    });
+    it("retreives elements from an SVG container with a resized viewBox", function() {
+        expect(svgroot instanceof SVGElement).toBe(true);
+        svgroot.setAttribute("viewBox", "0 0 60 60");
+        putandretrieve_circles();
+    });
+    it("retreives elements from an SVG container with a moved viewBox", function() {
+        expect(svgroot instanceof SVGElement).toBe(true);
+        svgroot.setAttribute("viewBox", "10 10 100 100");
+        putandretrieve_circles();
+    });
+    it("retreives elements from an SVG container with a non-trivial viewBox", function() {
+        expect(svgroot instanceof SVGElement).toBe(true);
+        svgroot.setAttribute("viewBox", "10 10 140 140");
+        putandretrieve_circles();
     });
 });
