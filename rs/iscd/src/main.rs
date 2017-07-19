ccceedb34b39484097d420ab6f2f18217d765cd1
Martin Broesamle <m@minusbroe.de>
2017-07-19 12:51:54 +0200

    ISCD integration tests (WASM/JS) pass.

--- rs/iscd/src/main.rs
+++ rs/iscd/src/main.rs
@@ -1,16 +1,157 @@
+extern crate nalgebra;
+extern crate ncollide;
+
+use std::cell::RefCell;
+use std::rc::Rc;
+use std::mem;
+
+use nalgebra::Point2;
+use ncollide::bounding_volume::AABB2;
+use ncollide::partitioning::{BoundingVolumeInterferencesCollector, DBVT, DBVTLeaf};
+
+type ElementId = i32;
+type ElementCount = i32;
+type Scalar = f32;
+type DataReg = DBVT<Point2<Scalar>, ElementId, AABB2<Scalar>>;
+type Leaf = DBVTLeaf<Point2<Scalar>, ElementId, AABB2<Scalar>>;
+type DataCollector<'a> = BoundingVolumeInterferencesCollector<'a, Point2<Scalar>, ElementId, AABB2<Scalar>>;
+
 #[no_mangle]
 pub extern fn magic_number() -> i32 {
     15432363
 }
 
 #[no_mangle]
-pub extern fn register_data_element(key: i32,
-                                    minx: i32,
-                                    miny: i32,
-                                    maxx: i32,
-                                    maxy: i32) -> i32 {
+pub extern fn magic_number_ptr() -> *mut i32 {
+    let magic_number: Box<i32> = Box::new(15432363);
+    Box::into_raw(magic_number)
+}
+
+#[no_mangle]
+pub extern fn destroy_magic_number_ptr(ptr: *mut i32) {
+    let _: Box<i32> = unsafe { Box::from_raw(ptr) };
+}
+
+#[no_mangle]
+pub extern fn new_data_reg () -> *mut DataReg {
+    let reg: Box<DataReg> = Box::new(DataReg::new());
+    Box::into_raw(reg)                          // avoid destruction (Box)
+}
+
+#[no_mangle]
+pub extern fn destroy_data_reg (reg_ptr: *mut DataReg) {
+    let _: Box<DataReg> = unsafe {
+        assert!(!reg_ptr.is_null());
+        Box::from_raw(reg_ptr)                  // Box from JS pointer
+    };
+}
+
+#[no_mangle]
+pub extern fn new_element_count () -> *mut ElementCount {
+    let reg: Box<ElementCount> = Box::new(0);
+    Box::into_raw(reg)                          // avoid destruction (Box)
+}
+
+#[no_mangle]
+pub extern fn destroy_element_count (count_ptr: *mut ElementCount) {
+    let _: Box<ElementCount> = unsafe {
+        assert!(!count_ptr.is_null());
+        Box::from_raw(count_ptr)                  // Box from JS pointer
+    };
+}
+
+#[no_mangle]
+pub extern fn new_vec(len_ptr: *mut ElementCount,
+                      capacity_ptr: *mut ElementCount)
+                      -> *mut ElementId {
+    let mut vec: Vec<ElementId> = Vec::new();
+    let mut len = unsafe { Box::from_raw(len_ptr) };            // Box from JS pointer
+    let mut capacity = unsafe { Box::from_raw(capacity_ptr) };  // Box from JS pointer
+    *len = vec.len() as ElementCount;
+    *capacity = vec.capacity() as ElementCount;
+    Box::into_raw(len);                                         // avoid destruction (Box)
+    Box::into_raw(capacity);                                    // avoid destruction (Box)
+    let vec_ptr = vec.as_mut_ptr();
+    mem::forget(vec);                           // avoid destruction (Vec)
+    vec_ptr
+}
+
+#[no_mangle]
+pub extern fn destroy_vec(vec_ptr: *mut ElementId,
+                          len_ptr: *mut ElementCount,
+                          capacity_ptr: *mut ElementCount)
+                          -> ElementId {
+    let len = unsafe { Box::from_raw(len_ptr) };            // Box from JS pointer
+    let capacity = unsafe { Box::from_raw(capacity_ptr) };  // Box from JS pointer
+    let mut vec: Vec<ElementId> = unsafe  {
+        Vec::from_raw_parts(vec_ptr, *len as usize, *capacity as usize)
+    };
+    Box::into_raw(len);                                         // avoid destruction (Box)
+    Box::into_raw(capacity);                                    // avoid destruction (Box)
+    let value = match vec.pop() {
+        Some(el) => el,
+        None => 99,
+    };
+    value
+}
+
+#[no_mangle]
+pub extern fn register_data_element(reg_ptr: *mut DataReg,
+                                    key: ElementId,
+                                    minx: Scalar,
+                                    miny: Scalar,
+                                    maxx: Scalar,
+                                    maxy: Scalar) -> *const RefCell<Leaf> {
     println!("ISCD register element: {} bounds: {}, {}, {}, {}", key, minx, miny, maxx, maxy);
-    0x00
+    let mut reg: Box<DataReg> = unsafe { Box::from_raw(reg_ptr) };          // Box from JS pointer
+    let mins = Point2::new(minx, miny);
+    let maxs = Point2::new(maxx, maxy);
+    let bbox: AABB2<Scalar> = AABB2::new(mins, maxs);
+    let reftoleaf: Rc<RefCell<Leaf>>= (*reg).insert_new(key, bbox);
+    Box::into_raw(reg);                 // avoid destruction (Box)
+    Rc::into_raw(reftoleaf)             // avoid destruction (Rc)
+}
+
+#[no_mangle]
+pub extern fn ungregister_and_destroy_leaf(reg_ptr: *mut DataReg,
+                                           leaf_ptr: *const RefCell<Leaf>) {
+    let mut leaf: Rc<RefCell<Leaf>> = unsafe { Rc::from_raw(leaf_ptr) };    // Rc from JS pointer
+    let mut reg: Box<DataReg> = unsafe { Box::from_raw(reg_ptr) };          // Box from JS pointer
+    (*reg).remove(&mut leaf);
+    Box::into_raw(reg);                                                     // avoid destruction (Box)
+}
+
+#[no_mangle]
+pub extern fn intersecting_data_elements(reg_ptr: *mut DataReg,
+                                         minx: Scalar,
+                                         miny: Scalar,
+                                         maxx: Scalar,
+                                         maxy: Scalar,
+                                         vec_ptr: *mut ElementId,
+                                         len_ptr: *mut ElementCount,
+                                         capacity_ptr: *mut ElementCount)
+                                         -> *mut ElementId {
+    // collector
+    let mins = Point2::new(minx, miny);
+    let maxs = Point2::new(maxx, maxy);
+    let mut bbox: AABB2<Scalar> = AABB2::new(mins, maxs);
+    let mut len = unsafe { Box::from_raw(len_ptr) };            // Box from JS pointer
+    let mut capacity = unsafe { Box::from_raw(capacity_ptr) };  // Box from JS pointer
+    let mut vec: Vec<ElementId> = unsafe  {     // Vec from JS pointer, len, capacity
+        Vec::from_raw_parts(vec_ptr, *len as usize, *capacity as usize)
+    };
+    vec.truncate(0);
+    // collect intersecting elements
+    let reg: Box<DataReg> = unsafe { Box::from_raw(reg_ptr) };  // Box from JS pointer
+    (*reg).visit(&mut DataCollector::new(&mut bbox, &mut vec));
+    Box::into_raw(reg);                                         // avoid destruction (Box)
+    *len = vec.len() as ElementCount;
+    *capacity = vec.capacity() as ElementCount;
+    Box::into_raw(len);                                         // avoid destruction (Box)
+    Box::into_raw(capacity);                                    // avoid destruction (Box)
+    let new_vec_ptr = vec.as_mut_ptr();
+    mem::forget(vec);                           // avoid destruction (Vec)
+    new_vec_ptr
 }
 
 // needed for rustc to compile to wasm
---
+++ spec/spec_iscd-wasm-module.js
@@ -0,0 +1,181 @@
+"use strict";
+
+describe("iscd.js, iscd.wasm", function () {
+    beforeEach(function(done) {
+        console.log("beforeEach, WASM_READY:", WASM_READY);
+        var waitForWASM = function () {
+            var retry_timer;
+            console.log("[SPEC] checking if WASM module is ready.");
+            if (WASM_READY) {
+                console.log("    WASM READY.");
+                done();
+            }
+            else {
+                console.log("    RETRY...");
+                retry_timer = window.setTimeout(waitForWASM, 50);
+            }
+        };
+        waitForWASM();
+    });
+    describe("._magic_number()", function () {
+        it("returns the magic number", function (done) {
+            expect(Module._magic_number()).toBe(0xeb7aab);
+            done();
+        });
+    });
+    describe("magic number via heap and pointer", function () {
+        var magic_number_ptr;
+        describe("._magic_number_ptr()", function () {
+            it("returns a pointer to the magic number", function (done) {
+                magic_number_ptr = Module._magic_number_ptr();
+                expect(typeof magic_number_ptr).toBe("number");
+                var magic_number = getValue(magic_number_ptr, "i32");
+                expect(magic_number).toBe(0xeb7aab);
+                done();
+            });
+        });
+        describe("._destroy_magic_number_ptr(ptr)", function () {
+            it("frees the memory with the magic number", function (done) {
+                expect(function () {
+                    Module._destroy_magic_number_ptr(magic_number_ptr);
+                }).not.toThrow();
+                done();
+            });
+        });
+    });
+    describe("USE CASE: Register and query data elements", function () {
+        var elementid = 143;
+        var data_reg_ptr = 0;
+        var vec_ptr = 0;
+        var capacity_ptr = 0;
+        var len_ptr = 0;
+        var leaf_ptr = 0;
+        describe("Create WASM data structures:", function () {
+            describe("._new_data_reg()", function () {
+                it("creates a data element registry and returns a pointer to it.", function (done) {
+                    data_reg_ptr = Module._new_data_reg();
+                    expect(typeof(data_reg_ptr)).toBe("number");
+                    done();
+                });
+            });
+            describe("._new_element_count()", function () {
+                it("allocates pointers to `ElementCount`.", function (done) {
+                    len_ptr = Module._new_element_count();
+                    capacity_ptr = Module._new_element_count();
+                    expect(typeof(len_ptr)).toBe("number");
+                    expect(typeof(capacity_ptr)).toBe("number");
+                    expect(len_ptr).not.toBe(capacity_ptr);
+                    done();
+                });
+            });
+            describe("._new_vec(len_ptr, capacity_ptr)", function () {
+                it("creates an empty `Vec`, updates `len_ptr` and `capacity_ptr`, returns a pointer.", function (done) {
+                    vec_ptr = Module._new_vec(capacity_ptr, len_ptr);
+                    expect(typeof(vec_ptr)).toBe("number");
+                    var len = getValue(len_ptr, 'i32');
+                    var capacity = getValue(capacity_ptr, 'i32');
+                    expect(len).toBe(0);
+                    expect(capacity).not.toBeLessThan(len);
+                    done();
+                });
+            });
+        });
+        describe("Data Element Registry", function () {
+            describe("._register_data_element(data_reg_ptr, elementid, 20, 40, 30, 50);", function () {
+                it("registers an element with a bounding box; returns a pointer to the corresponding `leaf`.", function (done) {
+                    leaf_ptr = Module._register_data_element(
+                                              data_reg_ptr,
+                                              elementid,
+                                              20, 40, 30, 50);
+                    expect(typeof(leaf_ptr)).toBe("number");
+                    done();
+                });
+            });
+            describe("._intersecting_data_elements(... , OUT vec_ptr, OUT capacity_ptr)", function () {
+                it("should return one element for query area 21, 31, 22, 32.", function (done) {
+                    vec_ptr = Module._intersecting_data_elements(
+                                              data_reg_ptr,
+                                              21, 41, 32, 52,
+                                              vec_ptr,
+                                              len_ptr,
+                                              capacity_ptr);
+                    var len = getValue(len_ptr, 'i32');
+                    var capacity = getValue(capacity_ptr, 'i32');
+                    expect(len).toBe(1);
+                    expect(capacity).not.toBeLessThan(len);
+                    done();
+                });
+                it("should return no element for query area 121, 131, 122, 132.", function (done) {
+                    vec_ptr = Module._intersecting_data_elements(
+                                              data_reg_ptr,
+                                              121, 131, 122, 132,
+                                              vec_ptr,
+                                              len_ptr,
+                                              capacity_ptr);
+                    var len = getValue(len_ptr, 'i32');
+                    var capacity = getValue(capacity_ptr, 'i32');
+                    expect(len).toBe(0);
+                    expect(capacity).not.toBeLessThan(len);
+                    done();
+                });
+            });
+            describe("._ungregister_and_destroy_leaf(data_reg_ptr, leaf_ptr)", function () {
+                it("removes a leaf from the registry and frees the allocated memory.", function (done) {
+                    expect(function () {
+                        Module._ungregister_and_destroy_leaf(data_reg_ptr, leaf_ptr);
+                    }).not.toThrow();
+                    done();
+                });
+            });
+            describe("._intersecting_data_elements( ... )", function () {
+                it("should no longer return an element for query area 21, 31, 22, 32.", function (done) {
+                    vec_ptr = Module._intersecting_data_elements(
+                                              data_reg_ptr,
+                                              21, 31, 22, 32,
+                                              vec_ptr,
+                                              len_ptr,
+                                              capacity_ptr);
+                    var len = getValue(len_ptr, 'i32');
+                    var capacity = getValue(capacity_ptr, 'i32');
+                    expect(len).toBe(0);
+                    expect(capacity).not.toBeLessThan(len);
+                    done();
+                });
+            });
+        });
+        describe("Destroy data structures (free allocated memory):", function () {
+            describe("._destroy_data_reg(data_reg_ptr)", function () {
+                it("destroys the registry (which must be cleared before to avoid memory leak).", function (done) {
+                    expect(function () {
+                        Module._destroy_data_reg(data_reg_ptr);
+                    }).not.toThrow();
+                    done();
+                });
+            });
+            describe("._destroy_vec(vec_ptr, len_ptr, capacity_ptr)", function () {
+                it("destroys a `Vec`; `len` and `capacity` as `ElementCount`.", function (done) {
+                    expect(function () {
+                        Module._destroy_vec(vec_ptr, len_ptr, capacity_ptr);
+                    }).not.toThrow();
+                    done();
+                });
+            });
+            describe("._destroy_element_count(len_ptr)", function () {
+                it("destroys the element count referred to by len_ptr.", function (done) {
+                    expect(function () {
+                        Module._destroy_element_count(len_ptr);
+                    }).not.toThrow();
+                    done();
+                });
+            });
+            describe("._destroy_element_count(capacity_ptr)", function () {
+                it("destroys the element count referred to by capacity_ptr.", function (done) {
+                    expect(function () {
+                        Module._destroy_element_count(capacity_ptr);
+                    }).not.toThrow();
+                    done();
+                });
+            });
+        });
+    });
+});
