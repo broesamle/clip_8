f2ee519d42513411fe476b0656ea11d591e72e3d
Martin Brösamle <m@martinbroesamle.de>
2020-07-17 18:03:03 +0200

    Code styling svg2demo.py; minor revisions.

--- bin/DemoTemplates.py
+++ bin/DemoTemplates.py
@@ -17,7 +17,6 @@
 #
 
 
-import PyBroeModules.MultiTemplateA as MT
 from string import Template
 
 from CommonTemplates import *
--- bin/svg2demo.py
+++ bin/svg2demo.py
@@ -50,80 +50,87 @@ class DemoPage(SVGGroupCollection):
         print("  OK!", el, el.tag)
         return
 
-print("\nBuilding the clip_8 Demo Pages")
-print("===================================================")
+if __name__ == "__main__":
+    print("\nBuilding the clip_8 Demo Pages")
+    print("===================================================")
 
-inDIRabs = os.path.join(CFG.rootDIRabs, CFG.demosDIR)
-outDIRabs = os.path.join(CFG.rootDIRabs, CFG.demosDIR)
-outext = CFG.demofile_ext
-tocsectionsHTML = ""
-alldemos = {}
-sectioncnt = 0
-firstoutfile = None
-firstsection = None
-SCT.demos.reverse()
-backlinktitle, backhref = "All Demos", "index.html"
-while len(SCT.demos) > 0:
-    chapter, infile = SCT.demos.pop()
-    outfile = os.path.splitext(infile)[0]+'.'+outext
-    if len(SCT.demos) > 0:
-        _nextlinktitle, _nexthref = SCT.demos[-1]
-        _nexthref = os.path.splitext(_nexthref)[0]+'.'+outext
-        nextlinkHTML = TEM.Linknext.substitute(href=_nexthref, linktext=_nextlinktitle)
-    else:
-        nextlinkHTML = ""
-    backlinkHTML = TEM.Linkback.substitute(href=backhref, linktext=backlinktitle)
-    if not firstoutfile:
-        # running in first round, only
-        firstoutfile = outfile
-        firstsection = chapter      ## Chapter and Section unified (for the demos)
-    inFN = os.path.join(inDIRabs, infile)
-    outFN = os.path.join(outDIRabs, outfile)
-    if os.path.isfile(inFN):
-        print("Processing:", inFN)
-        sectioncnt += 1
-        demopage = DemoPage(inFN, strictsubstitute=True)
-        alldemos[infile] = demopage
-        demopageHTML = demopage.generateSeries(itemTEM=TEM.Demo,
-                                                seriesTEM=TEM.Demos,
-                                                itemData={'viewBox': demopage.viewBox,
-                                                'width':"100%",
-                                                'height':"auto"})
-        footerHTML = TEM.Footer_str
-        bodyHTML = TEM.Body.substitute(pagetitle='<a href="index.html">clip_8</a>',
-                                        chapter=chapter, chaptercnt="Demos",
-                                        MAIN=demopageHTML,
-                                        link1=backlinkHTML, link2=nextlinkHTML,
-                                        FOOTER=footerHTML,
-                                        SCRIPT="")
-        clip8doc = Classic_Clip8Page(title="clip8 | " + chapter)
-        print ("    output:", outFN)
-        clip8doc.write_file(outFN, bodyHTML)
-        tocsectionsHTML += TEM.TOCsection.substitute(
-            demotitle=chapter,
-            demohref=outfile,
-            sectioncnt=sectioncnt)
-        backhref, backlinktitle = outfile, chapter
-    else:
-        print ("Sections.py refers to a non existing demo:", infile, "at path", inDIRabs)
+    inDIRabs = os.path.join(CFG.rootDIRabs, CFG.demosDIR)
+    outDIRabs = os.path.join(CFG.rootDIRabs, CFG.demosDIR)
+    outext = CFG.demofile_ext
+    tocsectionsHTML = ""
+    alldemos = {}
+    sectioncnt = 0
+    firstoutfile = None
+    firstsection = None
+    SCT.demos.reverse()
+    backlinktitle, backhref = "All Demos", "index.html"
+    while len(SCT.demos) > 0:
+        chapter, infile = SCT.demos.pop()
+        outfile = os.path.splitext(infile)[0]+'.'+outext
+        if len(SCT.demos) > 0:
+            _nextlinktitle, _nexthref = SCT.demos[-1]
+            _nexthref = os.path.splitext(_nexthref)[0]+'.'+outext
+            nextlinkHTML = TEM.Linknext.substitute(href=_nexthref,
+                                                   linktext=_nextlinktitle)
+        else:
+            nextlinkHTML = ""
+        backlinkHTML = TEM.Linkback.substitute(href=backhref,
+                                               linktext=backlinktitle)
+        if not firstoutfile:
+            # running in first round, only
+            firstoutfile = outfile
+            ### FIXME: cleanup terminology _chapter_, etc.
+            firstsection = chapter
+        inFN = os.path.join(inDIRabs, infile)
+        outFN = os.path.join(outDIRabs, outfile)
+        if os.path.isfile(inFN):
+            print("Processing:", inFN)
+            sectioncnt += 1
+            demopage = DemoPage(inFN, strictsubstitute=True)
+            alldemos[infile] = demopage
+            demopageHTML = demopage.generateSeries(
+                                itemTEM=TEM.Demo,
+                                seriesTEM=TEM.Demos,
+                                itemData={'viewBox': demopage.viewBox,
+                                'width':"100%",
+                                'height':"auto"})
+            footerHTML = TEM.Footer_str
+            bodyHTML = TEM.Body.substitute(
+                                pagetitle='<a href="index.html">clip_8</a>',
+                                chapter=chapter, chaptercnt="Demos",
+                                MAIN=demopageHTML,
+                                link1=backlinkHTML, link2=nextlinkHTML,
+                                FOOTER=footerHTML,
+                                SCRIPT="")
+            clip8doc = Classic_Clip8Page(title="clip8 | " + chapter)
+            print ("    output:", outFN)
+            clip8doc.write_file(outFN, bodyHTML)
+            tocsectionsHTML += TEM.TOCsection.substitute(
+                demotitle=chapter,
+                demohref=outfile,
+                sectioncnt=sectioncnt)
+            backhref, backlinktitle = outfile, chapter
+        else:
+            print ("Sections.py refers to a non existing demo:",
+                   infile, "at path", inDIRabs)
 
-### index.html
-### For the demos, index.html contains the TOC. There is no toc.html
-tocsectionsHTML = """
-<p>
-What you see can be seen on two levels of interpretation: (1) Graphics elements are visible to humans as any other SVG graphics on the web. (2) clip_8 interprets them as instructions of a purely visual programming language.
-</p>
-""" + tocsectionsHTML
-backlinkHTML = TEM.Linkback.substitute(href="https://github.com/broesamle/clip_8", linktext="Project page on github")
-nextlinkHTML = TEM.Linknext.substitute(href=firstoutfile, linktext=firstsection)
-footerHTML = TEM.FooterIndexpage_str
-bodyHTML = TEM.Body.substitute(pagetitle='clip_8',
-                               chapter="Demos", chaptercnt="Demos",
-                               MAIN=tocsectionsHTML,
-                               link1=backlinkHTML, link2=nextlinkHTML,
-                               FOOTER=footerHTML,
-                               SCRIPT="")
-outFN = os.path.join(outDIRabs, "index.html")
-clip8doc = Classic_Clip8Page(title="clip8 | Demos")
-print ("    output:", outFN)
-clip8doc.write_file(outFN, bodyHTML, supress_clip8scripts=True)
+    ### index.html
+    ### For the demos, index.html contains the TOC. There is no toc.html
+    backlinkHTML = TEM.Linkback.substitute(
+                        href="https://github.com/broesamle/clip_8",
+                        linktext="Project page on github")
+    nextlinkHTML = TEM.Linknext.substitute(href=firstoutfile,
+                                           linktext=firstsection)
+    footerHTML = TEM.FooterIndexpage_str
+    bodyHTML = TEM.Body.substitute(pagetitle='clip_8',
+                                   chapter="Demos",
+                                   chaptercnt="Demos",
+                                   MAIN=tocsectionsHTML,
+                                   link1=backlinkHTML,
+                                   link2=nextlinkHTML,
+                                   FOOTER=footerHTML,
+                                   SCRIPT="")
+    outFN = os.path.join(outDIRabs, "index.html")
+    clip8doc = Classic_Clip8Page(title="clip8 | Demos")
+    print ("    output:", outFN)
+    clip8doc.write_file(outFN, bodyHTML, supress_clip8scripts=True)
