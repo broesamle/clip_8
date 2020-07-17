d97ac18fa3a7c192207cbf8be05c4ca73d787a17
Martin Brösamle <m@martinbroesamle.de>
2020-07-17 18:06:24 +0200

    Change working directory in buildAll.py.

--- bin/buildAll.py
+++ bin/buildAll.py
@@ -1,4 +1,11 @@
+import os
 import subprocess
+oldwd = os.getcwd()
+newwd = os.path.dirname(os.path.realpath(__file__))
+print("changing working dir:", newwd)
+os.chdir(newwd)
 subprocess.run(["python", "buildTutorial.py"])
 subprocess.run(["python", "svg2refsheet.py"])
 subprocess.run(["python", "svg2demo.py"])
+print("restoring working dir:", oldwd)
+os.chdir(oldwd)
